82f95b320150efdd475da1512b37b2128b18b852
tyler1588 <87100297+tyler1588@users.noreply.github.com>
2023-08-05 13:28:59 -0400

    complete 6.9

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -2,7 +2,12 @@ import { useSelector, useDispatch } from 'react-redux'
 import { addVote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => {
+        if (state.filter === ''){
+            return state.anecdotes
+        }
+        return state.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(state.filter.toLowerCase()))
+    })
     const dispatch = useDispatch()
 
     const vote = (id) => {
