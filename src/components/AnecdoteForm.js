8bd2ef8f400062affe1b95211d210ab8694ec311
tyler1588 <87100297+tyler1588@users.noreply.github.com>
2023-08-05 12:51:51 -0400

    complete 6.8

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -12,10 +12,14 @@ const AnecdoteForm = () => {
     }
 
     return (
-        <form onSubmit={addAnecdote}>
-            <div><input name="anecdote" /></div>
-            <button type="submit">create</button>
-        </form>
+        <>
+            <h2>create new</h2>
+            <form onSubmit={addAnecdote}>
+                <div><input name="anecdote" /></div>
+                <button type="submit">create</button>
+            </form>
+        </>
+        
     )
 }
 
---
+++ src/components/AnecdoteList.js
@@ -0,0 +1,30 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { addVote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+    const anecdotes = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    const vote = (id) => {
+        dispatch(addVote(id))
+    }
+
+    return (
+        <>
+            <h2>Anecdotes</h2>
+            {anecdotes.map(anecdote =>
+                <div key={anecdote.id}>
+                <div>
+                    {anecdote.content}
+                </div>
+                <div>
+                    has {anecdote.votes}
+                    <button onClick={() => vote(anecdote.id)}>vote</button>
+                </div>
+                </div>
+            )}
+        </>
+    )
+}
+
+export default AnecdoteList
